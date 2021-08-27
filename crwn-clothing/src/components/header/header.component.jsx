e10028e2beb29bac727d400a404708fa7c00c4f1
manish4359 <manishkr.official4359@gmail.com>
2021-08-27 23:23:58 +0530

    signin and signout feature added

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom";
 
 import "./header.styles.scss";
 import { ReactComponent as Logo } from "./../../assets/logo.svg";
+import { auth } from "../../firebase/firebase.utils";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <link className="logo-container" to="/"></link>
     <Logo className="logo" />
@@ -15,6 +16,15 @@ const Header = () => (
       <Link className="option" to="/Contact">
         CONTACT
       </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
     </div>
   </div>
 );
